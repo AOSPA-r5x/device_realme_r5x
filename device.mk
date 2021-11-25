714094cf0aa71ec5aff255409a7bd7e7ad68cc94
mizdrake7 <mizdrake7@gmail.com>
2021-11-25 19:14:33 +0000

    r5x: scrpits: Rename Basebabd Fix

--- device.mk
+++ device.mk
@@ -267,7 +267,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    init.set_baseband.sh \
+    fix_baseband.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     init.perf.rc \
